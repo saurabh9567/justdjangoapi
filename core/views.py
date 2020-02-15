756aa0cb1c92516c771b07f3ec5b6747003408d1
saurabh <saurabh956@gmail.com>
2020-02-15 23:19:43 +0530

    api get and post method final update

--- core/views.py
+++ core/views.py
@@ -1,14 +1,21 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .sereializers import PostSerializer
+from .models import Post
 
 
 class TestView(APIView):
     def get(self, request, *args, **kwargs):
-        data = {
-                'name': 'john',
-                'age': 23
-        }
-        return Response(data)
+        qs = Post.objects.all()
+        serializer = PostSerializer(qs, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
 
 
 
