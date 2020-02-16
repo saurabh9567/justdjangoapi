496fdf60def90fc62bf0ff63655d44ef23500186
saurabh <saurabh956@gmail.com>
2020-02-16 11:14:54 +0530

    added django-rest-auth - in core/urls.py file and added app rest_auth in setting file

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from .views import TestView
 from rest_framework.authtoken.views import obtain_auth_token
 
@@ -6,4 +6,5 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('', TestView.as_view(), name='test'),
     path('api/token/', obtain_auth_token, name='obtain-token'),
+    path('rest-auth/', include('rest_auth.urls'))
 ]
