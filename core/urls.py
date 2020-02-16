84f162fd06f52b8fff1eca41d118631d98a0f3ab
saurabh <saurabh956@gmail.com>
2020-02-16 13:47:57 +0530

    using genericAPIView and mixins -- updated core\views and core/urls

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
-from .views import TestView
+from .views import PostView
 from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
-    path('', TestView.as_view(), name='test'),
+    path('', PostView.as_view(), name='test'),
     path('api/token/', obtain_auth_token, name='obtain-token'),
     path('rest-auth/', include('rest_auth.urls'))
 ]
--- core/views.py
+++ core/views.py
@@ -1,27 +1,55 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-
+from rest_framework import generics
+from rest_framework import mixins
 
 from .sereializers import PostSerializer
 from .models import Post
 
 
-class TestView(APIView):
-
-    permission_classes = (IsAuthenticated, )
+class PostView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    '''Shorter version of TestView -- commented below'''
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
 
     def get(self, request, *args, **kwargs):
-        qs = Post.objects.all()
-        serializer = PostSerializer(qs, many=True)
-        return Response(serializer.data)
+        return self.list(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
+        return self.create(request, *args, **kwargs)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class TestView(APIView):
+#
+#     permission_classes = (IsAuthenticated, )
+#
+#     def get(self, request, *args, **kwargs):
+#         qs = Post.objects.all()
+#         serializer = PostSerializer(qs, many=True)
+#         return Response(serializer.data)
+#
+#     def post(self, request, *args, **kwargs):
+#         serializer = PostSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors)
 
 
 
